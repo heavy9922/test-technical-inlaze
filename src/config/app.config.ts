e267a9c27c52811c54001140f191171c523fd248
Yeferson castiblanco <yefersoncasti@gmail.com>
2024-03-01 20:39:13 -0500

    fix: control an validation enviroments

---
+++ src/config/app.config.ts
@@ -0,0 +1,14 @@
+export const EnvConfiguration = () => ({
+  enviroment: process.env.NODE_ENV || 'dev',
+  dbHost: process.env.DB_HOST,
+  pgDb: process.env.POSTGRES_DB,
+  pgUser: process.env.POSTGRES_USER,
+  pgPasswd: process.env.POSTGRES_PASSWORD,
+  pgPort: process.env.PG_PORT,
+  port: process.env.PORT || 4000,
+  jwtSecret: process.env.JWT_SECRET,
+  redisUri: process.env.REDIS_URI,
+  redisPort: process.env.REDIS_PORT,
+  redisEnabed: process.env.REDIS,
+  hostApi: process.env.HOST_API,
+});
---
+++ src/config/joi.validation.ts
@@ -0,0 +1,15 @@
+import * as Joi from 'joi';
+
+export const JoiValidationSchema = Joi.object({
+  DB_HOST: Joi.string().required(),
+  POSTGRES_DB: Joi.string().required(),
+  POSTGRES_USER: Joi.string().required(),
+  POSTGRES_PASSWORD: Joi.string().required(),
+  PG_PORT: Joi.number().default(5432).required(),
+  PORT: Joi.number().default(3005).required(),
+  JWT_SECRET: Joi.string().required(),
+  REDIS_URI: Joi.string().required(),
+  REDIS_PORT: Joi.number().default(6379).required(),
+  REDIS: Joi.boolean().default(false).required(),
+  HOST_API: Joi.string().required(),
+});
